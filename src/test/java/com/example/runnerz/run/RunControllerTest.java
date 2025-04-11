8fcf923f013ddbd8d1338f5774b88beb45d17780
tmatkovic <toni.matkovic0@gmail.com>
2025-04-11 16:59:45 +0100

    added more unit tests, some need fixing

--- src/test/java/com/example/runnerz/run/RunControllerTest.java
+++ src/test/java/com/example/runnerz/run/RunControllerTest.java
@@ -3,18 +3,21 @@ package com.example.runnerz.run;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,5 +52,52 @@ class RunControllerTest {
 
     }
 
+    @Test
+    void shouldFindOneRun() throws Exception {
+        Run run = runs.getFirst();
+        when(repository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(run));
+        mvc.perform(get("/api/runs/1"))
+                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.id", is(Long.valueOf(run.id())))) TODO fix Long casting
+                .andExpect(jsonPath("$.title", is(run.title())))
+                .andExpect(jsonPath("$.miles", is(run.miles())))
+                .andExpect(jsonPath("$.location", is(run.location().toString())));
+    }
+
+    @Test
+    void shouldReturnNotFoundWithInvalidId() throws Exception {
+        mvc.perform(get("/api/runs/99"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreateNewRun() throws Exception {
+        var run = new Run(null, "test", LocalDateTime.now(), LocalDateTime.now().plusMinutes(15), 1, Location.INDOOR);
+        mvc.perform(post("/api/runs")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(run))
+                )
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldUpdateRun() throws Exception {
+        var run = new Run(null, "test", LocalDateTime.now(), LocalDateTime.now().plusMinutes(15), 1, Location.INDOOR);
+        mvc.perform(put("/api/runs/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(run))
+                )
+                .andExpect(status().isNoContent());
+    }
+
+
+//    @Test    //TODO fix
+//    public void shouldDeleteRun() throws Exception {
+//        mvc.perform(delete("/api/runs/1")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsString(runs.get(0)))
+//                )
+//                .andExpect(status().isNoContent());
+//    }
 
 }
