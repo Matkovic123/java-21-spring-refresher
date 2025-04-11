c1c1b52c89bf0f2d754db7ff5892104e003898fc
tmatkovic <toni.matkovic0@gmail.com>
2025-04-11 01:38:28 +0100

    Basic REST requets

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,9 +1,15 @@
 package com.example.runnerz;
 
+import com.example.runnerz.user.User;
+import com.example.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -14,12 +20,12 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner runner(RunRepository runRepository) {
-//        return args -> {
-//            Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR);
-//            runRepository.create(run);
-//        };
-//    }
+    @Bean
+    CommandLineRunner runner(UserRestClient client) {
+        return args -> {
+            List<User> users = client.findAll();
+            System.out.println(users);
+        };
+    }
 
 }
